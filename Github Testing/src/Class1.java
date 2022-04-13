174550894b2afc13f270d42bf77acbd0ab30092d
GLBro <30795132+GLBro@users.noreply.github.com>
2022-04-13 11:26:29 +0100

    Attempt 3

---
+++ Github Testing/src/Class1.java
@@ -0,0 +1,12 @@
+
+public class Class1 {
+	final int value = 1;
+	public void method1() {
+		System.out.println("Hello World");
+	}
+	
+	@SuppressWarnings("unused")
+	private static void method2() {
+		System.out.println("This only works in this class");
+	}
+}
---
+++ Github Testing/src/Class2.java
@@ -0,0 +1,10 @@
+
+public class Class2 {
+	static int value = 2;
+	protected void method1() {
+		System.out.println("Hello Universe");
+	}
+	public void method2() {
+		System.out.println("Instantiated from Class2");
+	}
+}
